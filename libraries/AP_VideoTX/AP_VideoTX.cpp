45f96eefb8181e120a2aa575841328cf3b0fdc32
Kostya <knifantiev@gmail.com>
2024-09-16 22:49:24 +0300

    Rearrange table

--- libraries/AP_VideoTX/AP_VideoTX.cpp
+++ libraries/AP_VideoTX/AP_VideoTX.cpp
@@ -212,17 +212,17 @@ const uint16_t AP_VideoTX::VIDEO_CHANNELS[AP_VideoTX::MAX_BANDS][VTX_MAX_CHANNEL
 AP_VideoTX::PowerLevel AP_VideoTX::_power_levels[VTX_MAX_POWER_LEVELS] = {
     // level, mw, dbm, dac
     { 0xFF,  0,    0, 0    }, // only in SA 2.1
-    { 0,    25,   14, 7    },
-    { 0x11, 100,  20, 0xFF }, // only in SA 2.1
-    { 1,    200,  23, 16   },
-    { 0x12, 400,  26, 0xFF }, // only in SA 2.1
+    { 1,    250,  25, 16   },
     { 2,    500,  27, 25   },
-    { 0x12, 600,  28, 0xFF }, // Tramp lies above power levels and always returns 25/100/200/400/600
-    { 3,    800,  29, 40   },
     { 0x13, 1000, 30, 0xFF }, // only in SA 2.1
     { 0x14, 2000, 34, 0XFF },  // slot reserved for a custom power level. Check actual power here
     { 0x15, 2500, 35, 0XFF },  // slot reserved for a custom power level. Check actual power here
     { 0x16, 3000, 36, 0XFF },  // slot reserved for a custom power level. Check actual power here
+    { 0,    25,   14, 7    },
+    { 0x11, 100,  20, 0xFF }, // only in SA 2.1
+    { 0x12, 400,  26, 0xFF }, // only in SA 2.1
+    { 0x12, 600,  28, 0xFF }, // Tramp lies above power levels and always returns 25/100/200/400/600
+    { 3,    800,  29, 40   },
 };
 
 
