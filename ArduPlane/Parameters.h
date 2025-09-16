bbf1133730005055e6ea9ab8e1e39e03b07f63ee
Kostya <knifantiev@gmail.com>
2025-09-16 10:16:31 +0300

    Returned one unused

--- ArduPlane/Parameters.h
+++ ArduPlane/Parameters.h
@@ -488,6 +488,9 @@ public:
     // var_info for holding Parameter information
     static const struct AP_Param::GroupInfo var_info[];
 
+    // just to make compilation easier when all things are compiled out...
+    uint8_t unused_integer;
+
     // button reporting library
 #if HAL_BUTTON_ENABLED
     AP_Button *button_ptr;
